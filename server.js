7b4467d4c6696bf56ae00e0ed4baa99b46c23245
Aalasyam Sai Varshith <85741790+sai045@users.noreply.github.com>
2022-12-09 11:12:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -203,7 +203,7 @@ app.post("/contact", async (req, res) => {
   );
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`server running at port ${PORT}`);
